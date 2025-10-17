e0cad9d995bc0bbb2e8c5666d868f36e7fd4ac3c
Abul Hassan <imabulhasan99@gmail.com>
2025-10-17 10:00:41 +0600

    Add social login support and routes for authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'email_verified_at',
+        'github_id',
+        'google_id',
     ];
 
     /**
