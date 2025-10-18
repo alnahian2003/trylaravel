775ed007bb13a0154f2668c22c146f3460964caa
Abul Hassan <imabulhasan99@gmail.com>
2025-10-18 13:53:58 +0600

    Remove production-only trustProxies call

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,10 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-        if (app()->environment('production')) {
-            $middleware->trustProxies(at: '*');
-        }
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
