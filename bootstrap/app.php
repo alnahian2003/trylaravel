ee6283e4721bb19e4228ee667580c08b71e2017a
Abul Hassan <imabulhasan99@gmail.com>
2025-10-18 13:48:28 +0600

    Force HTTPS and trust proxies in production

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        if (app()->environment('production')) {
+            $middleware->trustProxies(at: '*');
+        }
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
